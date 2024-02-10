5d715caf3dd43203887841e08440c1908b62ab8f
Atul Kumar <103561270+Atul99199@users.noreply.github.com>
2024-02-11 00:13:07 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <TodoList />
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TodoList,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-size: 16px;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background: linear-gradient(
+    135deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 9, 121, 1) 35%,
+    rgba(0, 212, 255, 1) 100%
+  );
+  color: rgb(2, 0, 36);
+  width: 100vw;
+  height: 100vh;
+  padding: 50px;
+}
+</style>
